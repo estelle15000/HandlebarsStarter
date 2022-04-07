c16c56109ffe4bdeaca73fcd05a5082dafdcee11
ESTELLE <estelle.huleux@hotmail.com>
2022-04-07 19:37:28 +0200

    FIX

--- routes/logout.js
+++ routes/logout.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const middleware = require("../middleware/middleware.js");
 router.get('/', function (req, res, next) {
     // -------------------------------
-    // DESTROY NODE SERVER SESSION (NO JWT TOKEN) - FIVERR
+    // DESTROY NODE SERVER SESSION (NO JWT TOKEN)
     // ------------------------------- 
     req.session.authenticated = false;
     // -------------------------------
-    // SWITCH LOG IN TO LOG OUT IN NAVBAR - FIVERR
+    // SWITCH LOG IN TO LOG OUT IN NAVBAR 
     // ------------------------------- 
     global.auth = false;
     // -------------------------------
-    // REDIRECT TO HOME - FIVERR
+    // REDIRECT TO HOME 
     // ------------------------------- 
     res.redirect('../home');
 })
